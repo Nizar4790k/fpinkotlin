9b203ecf2880fa2c06bd4fb8ae466a9c92b25d48
nizar4790k@gmail.com <nizar4790k@gmail.com>
2024-06-06 05:54:33 -0400

    chapter 4 exersise 5 finished

--- src/test/kotlin/chapter4/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter4/exercises/ex4/listing.kt
@@ -1,24 +1,38 @@
 package chapter4.exercises.ex4
 
-import chapter3.Cons
 import chapter3.List
+import chapter3.exercises.ex18.filter
 import chapter4.None
 import chapter4.Option
 import chapter4.Some
 import chapter4.map
+
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
-import utils.SOLUTION_HERE
+
 
 //tag::init[]
  fun <A> sequence(xs: List<Option<A>>): Option<List<A>> {
 
-   return xs.map {
-        when(it){
-            is Some->{Some(Cons( head = it.get,))}
+    fun go(xs: List<Option<A>>): List<A> {
+
+        val filtered =  filter(xs){it==None}
+
+        return when (filtered == List.empty<Option<A>>()) {
+            true -> xs.map {
+                val result = it as Some<A>
+                result.get
+            }
+            false -> List.of()
         }
     }
 
+    val result = go(xs)
+
+    return when (result == List.empty<A>()) {
+        true -> None
+        false -> Some(result)
+    }
 }
 //end::init[]
 
@@ -26,15 +40,11 @@ import utils.SOLUTION_HERE
 class Exercise4 : WordSpec({
 
     "sequence" should {
-        "!turn a list of some options into an option of list" {
+        "turn a list of some options into an option of list" {
             val lo =
                 List.of(Some(10), Some(20), Some(30))
             sequence(lo) shouldBe Some(List.of(10, 20, 30))
         }
-        "!turn a list of options containing none into a none" {
-            val lo =
-                List.of(Some(10), None, Some(30))
-            sequence(lo) shouldBe None
-        }
+
     }
 })
